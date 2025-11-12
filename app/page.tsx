e74e995395ed00151741c0f6d07539c0912c1a8b
Lijucay <lijucayy@proton.me>
2025-11-12 06:22:47 +0100

    Replacing old link-tag with updated Button component Replacing favicon with the actual icon

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,16 @@ import AboutMe from "@/app/about/aboutme";
 import logo from "@/public/logo_icon.png";
 import Qwotable from "@/app/qwotable/qwotable";
 import Damier from "@/app/damier/damier";
+import Button from "@/app/components/Button";
 
 export default function Home() {
   return (
       <div>
           <header className="flex flex-row items-center justify-between w-4/5 mx-auto mt-5">
               <Image src={logo.src} width={30} height={30} alt="logo"/>
-              <div className="rounded-full bg-black text-white py-2 px-4 w-fit">
-                  <a href="https://blog.lijucay.de/">
-                      <p className="inline-block">Blog</p>
-                  </a>
-              </div>
+              <Button href="https://blog.lijucay.de/" className="bg-black text-white">
+                  Blog
+              </Button>
           </header>
           <section id="aboutme">
               <AboutMe/>
